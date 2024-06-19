c968310b88240ffcd9f147ffa4f1ac9add199d1f
Dzaky F <dfiantino@gmail.com>
2024-06-19 10:20:50 +0700

    Add Throw Food Feature

---
+++ Assets/Scripts/FoodController.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class FoodController : MonoBehaviour
+{
+    public float speed = 5;
+    public float destroyAfter = 20;
+
+    void Start()
+    {
+        Destroy(gameObject, destroyAfter);
+    }
+
+    void Update()
+    {
+        transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z + speed);
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,22 +6,33 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float speed = 0.5f;
-    private CharacterController characterController;
+    public Transform foodPrefab;
+
     private Animator playerAnimator;
 
     void Awake()
     {
-        characterController = GetComponent<CharacterController>();
         playerAnimator = GetComponent<Animator>();
     }
 
     void Update() 
     {
-        Vector3 move = new Vector3(Input.GetAxis("Horizontal"),0,0);
-        characterController.Move(move * speed * Time.deltaTime);
+        Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0 , 0);
+        transform.position += move * speed * Time.deltaTime;
 
         if (move.x > 0) playerAnimator.Play("WalkRight");
         else if (move.x < 0) playerAnimator.Play("WalkLeft");
         else playerAnimator.Play("Idle");
+
+        if (Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            ThrowFood();
+        }
+    }
+
+    void ThrowFood()
+    {
+        playerAnimator.Play("ThrowFood");
+        Instantiate(foodPrefab, this.transform.position, Quaternion.identity);
     }
 }
